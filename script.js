567d82bd374f3a94a86837e997e7ed0aae5050bb
RoboKitty <robokitty@tutamail.com>
2024-06-19 23:24:37 -0400

    Update RoboKitty website with new HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example of a simple animation script
+document.addEventListener('DOMContentLoaded', () => {
+    const robokittyImage = document.querySelector('#robokitty-intro img.animated');
+    robokittyImage.classList.add('blink');
+});
